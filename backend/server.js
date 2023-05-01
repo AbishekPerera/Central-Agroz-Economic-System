17f6f9b2dcb549f3d747032ceaa3424b2bbc6a5e
AbishekPerera <abhishekperera77@gmail.com>
2023-05-02 00:16:11 +0530

    init

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+const app = express();
+
+const PORT = process.env.PORT || 8075;
+app.use(cors());
+app.use(bodyParser.json());
+
+dotenv.config();
+
+const URL = process.env.MONGODB_URL;
+
+mongoose
+  .connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    })
+  )
+  .catch((err) => console.log(err));
+
+//routes-----------------------------------------------------------------
---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import HomePage from "./pages/HomePage/HomePage";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
