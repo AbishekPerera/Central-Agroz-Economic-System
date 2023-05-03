1c85af69a46519856942e704c2b64dfef45eb5d0
LochaniRanasinghe <lochaniranasinghe2000@gmail.com>
2023-05-03 23:24:43 +0530

    Create jsx

---
+++ frontend/src/pages/System/FarmerPages/FarmerLogin.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../../../components/Header/Header'
+import Footer from '../../../components/Footer/Footer'
+
+const FarmerLogin = () => {
+  return (
+    <div>
+        <Header/>
+        FarmerLogin
+        <Footer/>    
+    </div>
+
+  )
+}
+
+export default FarmerLogin
