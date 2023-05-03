7d812e8af6149d70d388ce9ff171052fc1f5b391
LochaniRanasinghe <lochaniranasinghe2000@gmail.com>
2023-05-03 23:24:22 +0530

    Create jsx

---
+++ frontend/src/pages/System/FarmerPages/FarmerDashboard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Styles/FarmerDashbord.css";
+import Header from '../../../components/Header/Header'
+import Footer from '../../../components/Footer/Footer'
+import FarmerSidebar from '../../../components/FarmerSidebar/FarmerSidebar'
+
+const FarmerDashboard = () => {
+  return (
+    <div>
+        <Header/>
+        <br/>
+        <div className="mainContainer">
+            <div className="sidebar">
+            <FarmerSidebar/>
+            </div>
+
+            <div className="content">
+                FarmerDashboard
+            </div>
+        </div>    
+        <Footer/>    
+    </div>
+  )
+}
+
+export default FarmerDashboard
