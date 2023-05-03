5f8bb1c1d102ce5bd6786b0f72f815e677125129
LochaniRanasinghe <lochaniranasinghe2000@gmail.com>
2023-05-03 23:25:00 +0530

    create jsx

---
+++ frontend/src/pages/System/FarmerPages/FarmerPublishProducts.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../../components/Header/Header'
+import Footer from '../../../components/Footer/Footer'
+
+const FarmerPublishProducts = () => {
+  return (
+    <div>
+        <Header/>
+        FarmerPublishProducts
+        <Footer/>
+    </div>
+  )
+}
+
+export default FarmerPublishProducts
