c4c496732dc1e71ef5b589e60821c9d73df986c6
LochaniRanasinghe <lochaniranasinghe2000@gmail.com>
2023-05-03 23:20:45 +0530

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import HomePage from "./pages/HomePage/HomePage";
 import { Route, Routes } from "react-router-dom";
 import AboutUsPage from "./pages/AboutUsPage/AboutUsPage";
 import NewsPage from "./pages/NewsPage/NewsPage";
+import FarmerDashboard from "./pages/System/FarmerPages/FarmerDashboard";
+import FarmerLogin from "./pages/System/FarmerPages/FarmerLogin";
+import FarmerProfile from "./pages/System/FarmerPages/FarmerProfile";
+import FarmerPublishProducts from "./pages/System/FarmerPages/FarmerPublishProducts";
 
 function App() {
   return (
@@ -12,6 +16,12 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutUsPage />} />
         <Route path="/news" element={<NewsPage />} />
+
+        {/* Farmer Routes */}
+        <Route path="/system/farmer-farmerdashboard" element={<FarmerDashboard />} />
+        <Route path="/system/farmer-farmerlogin" element={<FarmerLogin />} />
+        <Route path="/system/farmer-farmerprofile" element={<FarmerProfile />} />
+        <Route path="/system/farmer-publishproducts" element={<FarmerPublishProducts />} />
       </Routes>
     </div>
   );
