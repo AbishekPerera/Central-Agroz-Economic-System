3c01881aed6bcc74da7414da7a575cf9fbb2dc0e
Kavindya-Udunuwara <udunuwaraukc@gmail.com>
2023-05-06 12:44:28 +0530

    create backend controllers

---
+++ backend/controllers/ECMOControllers/StockQuantity.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const quantitySchema = new Schema({
+
+  CenterName: {
+    type: String,
+    required: true,
+  },
+
+  Category: {
+    type: String,
+    enum: ["Vegetable", "Fruit", "Grain", "Rice"],
+    required: true,
+  },
+
+  Type: {
+    type: String,
+    required: true,
+  },
+
+  BoughtQuantity: {
+    type: Number,
+    default: 0,
+  },
+
+  SoldQuantity: {
+    type: Number,
+    default: 0,
+  },
+
+  AvailableQuantity: {
+    type: Number,
+    default: 0,
+  },
+
+  Date: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+const Quantity = mongoose.model("StockQuantity", quantitySchema);
+
+export default Quantity;
