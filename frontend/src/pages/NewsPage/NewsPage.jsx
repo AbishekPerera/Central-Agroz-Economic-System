2f89409a46ff1bcbbef2ff3ee24821bdaee4b92b
LochaniRanasinghe <lochaniranasinghe2000@gmail.com>
2023-05-07 18:53:24 +0530

    modify code

--- frontend/src/pages/NewsPage/NewsPage.jsx
+++ frontend/src/pages/NewsPage/NewsPage.jsx
@@ -39,33 +39,18 @@ const NewsPage = () => {
         <div className="news-page-lowerbody">
           <Row xs={1} md={2} lg={3} className="g-4">
             {/* add grid classes */}
-            <Col key={1}>
+            <Col key={5}>
               <Card style={{ width: "25rem" }}>
                 <Card.Img
                   variant="top"
-                  src="https://store.spaceylon.com/wp-content/uploads/2022/06/bath-and-shower-gels.jpg"
+                  src="https://i.pinimg.com/564x/ba/35/f9/ba35f94cf7e7d3ba6d202579341ade48.jpg"
                 />
                 <Card.Body>
-                  <Card.Title>New Product Launches</Card.Title>
+                  <Card.Title>Central Agroz announces new line of organic fertilizers</Card.Title>
                   <Card.Text>
-                  Discover our latest additions to our natural skincare line! Say goodbye to harsh chemicals and try our new herbal shampoos or our anti-aging cream with all-natural ingredients. Shop now and get the best of Ayurvedic skincare.
-                  </Card.Text>
-                  <Button variant="primary">Read More</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col key={2}>
-              <Card style={{ width: "25rem" }}>
-                <Card.Img
-                  variant="top"
-                  src="https://www.lankahospitals.com/wp-content/uploads/2022/03/Lanka-Hospitals-Academy-partners-with-National-Youth-Corp1-1024x1024-1.jpg"
-                />
-                <Card.Body>
-                  <Card.Title>Community Outreach and Charity</Card.Title>
-                  <Card.Text>
-                  Join us in giving back to our community! Partnered with a local charity, we are offering free Ayurvedic consultations to low-income individuals. Attend our community yoga event in the park this weekend and all proceeds will be donated to a local nonprofit organization.
-                  </Card.Text>
-                  <Button variant="primary">Read More</Button>
+                     Central Agroz has announced the launch of a new line of organic fertilizers, made from all-natural ingredients. These fertilizers are designed to provide the essential nutrients that plants need to thrive, while also promoting soil health and reducing the environmental impact of agriculture. We believe that these fertilizers will be an important tool for farmers who are looking to adopt more sustainable practices.                  
+                     <Button variant="primary">Read More</Button>
+                  </Card.Text>   
                 </Card.Body>
               </Card>
             </Col>
@@ -73,62 +58,75 @@ const NewsPage = () => {
               <Card style={{ width: "25rem" }}>
                 <Card.Img
                   variant="top"
-                  src="https://lk.spaceylon.com/wp-content/uploads/2023/03/14-days-of-spring-wellness-beauty-calendar.jpg"
+                  src="https://i.pinimg.com/564x/12/2f/23/122f2357785413aa1c15c34c180c254c.jpg"
                 />
                 <Card.Body>
-                  <Card.Title>Promotions and Discounts</Card.Title>
+                  <Card.Title>Central Agroz introduces new system to reduce food waste</Card.Title>
                   <Card.Text>
-                  Take advantage of our amazing deals this month! Enjoy 20% off on all cosmetic products or grab three skincare products for the price of two. Shop now and save big!
-                  </Card.Text>
+                  Central Agroz is committed to reducing food waste in Sri Lanka, and we are pleased to announce the introduction of a new system to track the sales and inventory at economic centers. By monitoring the demand for each crop, we can help farmers produce only what is needed, reducing excess inventory and preventing unsold food from being discarded.                  </Card.Text>
                   <Button variant="primary">Read More</Button>
                 </Card.Body>
               </Card>
             </Col>
+           
+            <Col key={2}>
+              <Card style={{ width: "25rem" }}>
+                <Card.Img
+                  variant="top"
+                  src="https://i.pinimg.com/564x/1d/38/82/1d3882e44a28486e8103ef2c4fdfde1d.jpg"
+                />
+                <Card.Body>
+                  <Card.Title>Sri Lanka experiences record-breaking rice harvest</Card.Title>
+                  <Card.Text>
+                  According to the Department of Agriculture, Sri Lanka has achieved its highest-ever rice harvest this year, with a production of over 4.5 million metric tons. This is great news for Sri Lanka's farmers and the country's food security, and Central Agroz is proud to support the growth of the agriculture industry.                  </Card.Text>
+                  <Button variant="primary">Read More</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col key={1}>
+              <Card style={{ width: "25rem" }}>
+                <Card.Img
+                  variant="top"
+                  src="https://i.pinimg.com/564x/ec/e6/b6/ece6b65458dc339c5642fa4f9fadbe81.jpg"
+                />
+                <Card.Body>
+                  <Card.Title>Central Agroz announces new partnership with major export company</Card.Title>
+                  <Card.Text>
+                  Central Agroz is excited to announce our recent partnership with XYZ Export Company, which will allow our farmers to sell their crops directly to international markets. This partnership will provide new business opportunities for our farmers and contribute to the growth of Sri Lanka's economy.                  </Card.Text>
+                  <Button variant="primary">Read More</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            
             <Col key={4}>
               <Card style={{ width: "25rem" }}>
                 <Card.Img
                   variant="top"
-                  src="https://media.istockphoto.com/id/1156263061/photo/attractive-young-woman-exercising-and-sitting-in-yoga-lotus-position-while-resting-at-home.jpg?s=612x612&w=0&k=20&c=Sr5y1ByF1cLJXjkxYLyir-g0MSgHJMw-pumWtsT0sfc="
+                  src="https://i.pinimg.com/236x/91/ac/57/91ac5704e2eda1ab1f504c7b03a9e455.jpg"
                 />
                 <Card.Body>
-                  <Card.Title>Ayurvedic Lifestyle and Practices</Card.Title>
+                  <Card.Title>Meet Nimal, a dedicated farmer from Hambantota</Card.Title>
                   <Card.Text>
-                  Dive into the world of Ayurveda with our Ayurvedic lifestyle and practices section. Learn how to understand the doshas and how to balance them or the importance of meditation in Ayurveda.
-                  </Card.Text>
+                  Nimal has been farming for over 20 years, and his hard work and dedication have paid off with a successful harvest this season. With the help of Central Agroz, he was able to sell his crops directly to customers and earn a fair price for his hard work. We are proud to support farmers like Nimal and help them succeed in the agriculture industry.                  </Card.Text>
                   <Button variant="primary">Read More</Button>
                 </Card.Body>
               </Card>
             </Col>
-            <Col key={5}>
-              <Card style={{ width: "25rem" }}>
-                <Card.Img
-                  variant="top"
-                  src="https://lk.spaceylon.com/wp-content/uploads/2021/03/Best-Moisturiser-For-Your-Skin-Type-570x400-1.jpg"
-                />
-                <Card.Body>
-                  <Card.Title>Health and Wellness Tips</Card.Title>
-                  <Card.Text>
-                  Get expert tips on how to take care of your skin and overall well-being with our health and wellness tips. Learn how to maintain healthy skin during winter or the benefits of drinking herbal tea for digestion.
-                  </Card.Text>
-                  <Button variant="primary">Read More</Button>
-                </Card.Body>
-              </Card>
-            </Col>
+            
             <Col key={6}>
               <Card style={{ width: "25rem" }}>
                 <Card.Img
                   variant="top"
-                  src="https://lk.spaceylon.com/wp-content/uploads/2017/08/Neem-amp-Tea-Tree-Overnight-Treatment-Masque-100g-OG.jpg"
+                  src="https://s3.amazonaws.com/bizenglish/wp-content/uploads/2019/10/15170839/COEXIST-Foundation-together-with-Sarvodaya-Development-Finance-successfully-carry-out-Value-Creation-Workshop.png"
                 />
                 <Card.Body>
-                  <Card.Title>New Product Launches</Card.Title>
+                  <Card.Title>Central Agroz hosts seminar on the benefits of regenerative agriculture</Card.Title>
                   <Card.Text>
-                  Discover our latest additions to our natural skincare line! Say goodbye to harsh chemicals and try our new herbal shampoos or our anti-aging cream with all-natural ingredients. Shop now and get the best of Ayurvedic skincare.
-                  </Card.Text>
+                    Central Agroz recently hosted a seminar on the benefits of regenerative agriculture, a practice that focuses on rebuilding soil health and increasing biodiversity. The seminar featured expert speakers who discussed the latest research on regenerative agriculture, as well as practical tips for farmers who are looking to adopt these practices. We believe that regenerative agriculture has the potential to transform the industry and create a more sustainable future for farming.                  </Card.Text>
                   <Button variant="primary">Read More</Button>
                 </Card.Body>
               </Card>
-            </Col>
+            </Col> 
             
           </Row>
         </div>
